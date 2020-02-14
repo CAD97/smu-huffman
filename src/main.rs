9558f5cbc967f900883d8f4d878750317bb94310
CAD97 <cad97@cad97.com>
2020-02-14 15:03:06 -0500

    Give up on piping because it's broken

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,65 @@ fn main() -> io::Result<()> {
     let mut args = env::args();
 
     // Usage
-    if args.len() != 3 {
+    if args.len() != 4 {
         print_usage();
     }
 
     let _exe_name = args.next().unwrap();
-    let output = match &*args.next().unwrap() {
-        "compress" => process(&args.next().unwrap(), compress),
-        "decompress" => process(&args.next().unwrap(), decompress),
+    match &*args.next().unwrap() {
+        "compress" => process(&args.next().unwrap(), &args.next().unwrap(), compress),
+        "decompress" => process(&args.next().unwrap(), &args.next().unwrap(), decompress),
+        "test" => {
+            if &*args.next().unwrap() != "roundtrip" {
+                print_usage();
+            }
+            test(&args.next().unwrap())
+        },
         _ => print_usage(),
-    }?;
-
-    io::stdout().write_all(&output)
+    }
 }
 
 fn print_usage() -> ! {
     println!("Usage:");
-    println!("  smu-huffman compress <path>           Compress a file and write it to stdout");
-    println!("  smu-huffman compress -                Compress stdin and write it to stdout");
-    println!("  smu-huffman decompress <path>         Decompress a file and write it to stdout");
-    println!("  smu-huffman decompress -              Decompress stdin and write it to stdout");
+    println!("  smu-huffman compress <input path> <output path>");
+    println!("  smu-huffman decompress <input path> <output path>");
+    println!("  smu-huffman test roundtrip <input path>");
+    println!("To read/write to stdin/stdout, use `-` as the path.");
     process::exit(1)
 }
 
-fn process(path: &str, op: impl for<'a> FnOnce(&'a [u8]) -> Vec<u8>) -> io::Result<Vec<u8>> {
-    let buf = if path == "-" {
+fn process(input_path: &str, output_path: &str, op: impl for<'a> FnOnce(&'a [u8]) -> Vec<u8>) -> io::Result<()> {
+    let input_bytes = read(input_path)?;
+    let output_bytes = op(&input_bytes);
+    write(output_path, &output_bytes)
+}
+
+fn read(input_path: &str) -> io::Result<Vec<u8>> {
+    if input_path == "-" {
         let mut buf = Vec::new();
         io::stdin().read_to_end(&mut buf)?;
-        buf
+        Ok(buf)
     } else {
-        fs::read(path)?
-    };
-    Ok(op(&buf))
+        fs::read(input_path)
+    }
+}
+
+fn write(output_path: &str, bytes: &[u8]) -> io::Result<()> {
+    if output_path == "-" {
+        io::stdout().write_all(&bytes)
+    } else {
+        fs::write(output_path, &bytes)
+    }
+}
+
+fn test(input_path: &str) -> io::Result<()> {
+    let input_bytes = read(input_path)?;
+    let roundtrip_bytes = decompress(&compress(&input_bytes));
+    if input_bytes == roundtrip_bytes {
+        println!("passed");
+    } else {
+        println!("failed");
+        process::exit(1);
+    }
+    Ok(())
 }
